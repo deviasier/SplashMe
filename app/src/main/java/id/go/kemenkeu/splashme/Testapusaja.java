82d7ca808850b3f977fcc495e34b2053ad41f4a7
DhoniSFA <53330502+DhoniSFA@users.noreply.github.com>
2019-08-02 14:32:37 +0700

    tambah class

---
+++ app/src/main/java/id/go/kemenkeu/splashme/Testapusaja.java
@@ -0,0 +1,14 @@
+package id.go.kemenkeu.splashme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Testapusaja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testapusaja);
+    }
+}
